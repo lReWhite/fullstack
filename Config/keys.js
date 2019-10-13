8133792a7f7c13a8694809c3d3447ab25dbc9fbd
Denis Isachenko <55709662+lReWhite@users.noreply.github.com>
2019-10-14 00:45:39 +0300

    key

--- Config/keys.js
+++ Config/keys.js
@@ -1,9 +1,9 @@
-// if(process.env.NODE_ENV === 'production'){
-//     module.exports = require('./keys.prod')
-// } else {
-//     module.exports = require('./keys.dev')
-// }
-module.exports ={
-    mongoURI:'mongodb+srv://ReWhite:senefera@fullstack-qfzip.mongodb.net/test?retryWrites=true&w=majority',
-    jwt: 'dev-jwt'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
+// module.exports ={
+//     mongoURI:'mongodb+srv://ReWhite:senefera@fullstack-qfzip.mongodb.net/test?retryWrites=true&w=majority',
+//     jwt: 'dev-jwt'
+// }
