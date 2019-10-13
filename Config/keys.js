b605ae409c4aa5d39b04fa314e82b215e4f61818
Denis Isachenko <55709662+lReWhite@users.noreply.github.com>
2019-10-13 16:58:47 +0300

    keys.

--- Config/keys.js
+++ Config/keys.js
@@ -1,4 +1,5 @@
-module.exports ={
-    mongoURI:'mongodb+srv://ReWhite:senefera@fullstack-qfzip.mongodb.net/test?retryWrites=true&w=majority',
-    jwt: 'dev-jwt'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const passport = require('passport')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const path = require('path')
 const authRoutes =require('./routes/auth')
 const analyticsRoutes =require('./routes/analytics')
 const categoryRoutes =require('./routes/category')
@@ -27,5 +28,18 @@ app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
 
+
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/dist/client'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(
+            path.resolve(
+                __dirname,'client','dist','client','index.html'
+            )
+        )
+    })
+}
 
 module.exports = app
