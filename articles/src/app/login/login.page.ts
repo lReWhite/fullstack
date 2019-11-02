21d4c3ea9b5aa106e1ce87ba03fba0c887a5f358
Denis Isachenko <55709662+lReWhite@users.noreply.github.com>
2019-11-02 14:24:08 +0300

    Логин перенес в другой файл.

--- articles/src/app/login/login.page.ts
+++ articles/src/app/login/login.page.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from "../../../../client/src/app/shared/services/auth.service";
+import {MaterialService} from "../../../../client/src/app/shared/classes/material.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, OnDestroy{
+  form: FormGroup
+  aSub: Subscription
 
-  constructor() { }
+
+
+  constructor(private auth:AuthService, private router: Router, private route: ActivatedRoute) {
+
+  }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email:new FormControl(null,[Validators.required, Validators.email]),
+      password: new FormControl(null , [Validators.required, Validators.minLength(6)])
+    })
+    this.route.queryParams.subscribe((params: Params) =>{
+      if(params['registered']){
+        MaterialService.toast('Теперь вы можете зайти в систему используя свои данные')
+
+      } else if (params['accessDenied']) {
+        MaterialService.toast('Для начала авторизуйтесь в системе')
+
+      }else if(params['sessionFiled']){
+        MaterialService.toast('Пожалуйста войдите в систему заного')
+      }
+    })
+  }
+  ngOnDestroy() {
+    if(this.aSub){
+      this.aSub.unsubscribe()
+    }
+
+  }
+
+  onSubmit(){
+    this.form.disable()
+
+    this.aSub=this.auth.login(this.form.value).subscribe(
+        () => this.router.navigate(['/overview']),
+        error => {
+          MaterialService.toast(error.error.message)
+
+          this.form.enable()
+        }
+    )
   }
 
 }
