09eb61a3d59508bb9cc927e9df3b3c1ad5adaa68
KiTKasatka <57257022+KiTKasatka@users.noreply.github.com>
2019-11-02 15:23:04 +0300

    Перекинул Html с клиента на articles. Для register и analitics.

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -9,6 +9,7 @@ import {MaterialService} from "../shared/classes/material.service";
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
+
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit, OnDestroy{
