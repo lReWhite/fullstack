88a6a3c8bd3f026748c3b95c097c230744c21d5e
Denis Isachenko <55709662+lReWhite@users.noreply.github.com>
2019-11-02 15:30:16 +0300

    удалил логин

--- articles/src/app/login/login.page.ts
+++ articles/src/app/login/login.page.ts
@@ -10,7 +10,50 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit, OnDestroy{
+  form: FormGroup
+  aSub: Subscription
 
+
+
+  constructor(private auth:AuthService, private router: Router, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email:new FormControl(null,[Validators.required, Validators.email]),
+      password: new FormControl(null , [Validators.required, Validators.minLength(6)])
+    })
+    this.route.queryParams.subscribe((params: Params) =>{
+      if(params['registered']){
+        MaterialService.toast('Теперь вы можете зайти в систему используя свои данные')
+
+      } else if (params['accessDenied']) {
+        MaterialService.toast('Для начала авторизуйтесь в системе')
+
+      }else if(params['sessionFiled']){
+        MaterialService.toast('Пожалуйста войдите в систему заного')
+      }
+    })
+  }
+  ngOnDestroy() {
+
+
+  }
+
+  onSubmit(){
+    this.form.disable()
+
+   this.auth.login(this.form.value).subscribe(
+        () => this.router.navigate(['/overview']),
+        error => {
+          MaterialService.toast(error.error.message)
+
+          this.form.enable()
+        }
+    )
+  }
 
 }
+
