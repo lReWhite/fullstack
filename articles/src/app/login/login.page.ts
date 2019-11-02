0e056ad1ba8478f67fdd10ca4c4723e8fa46af3f
Denis Isachenko <55709662+lReWhite@users.noreply.github.com>
2019-11-02 16:09:57 +0300

    2

--- articles/src/app/login/login.page.ts
+++ articles/src/app/login/login.page.ts
@@ -10,41 +10,7 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit, OnDestroy{
-
-form : FormGroup
-
-  constructor(private auth:AuthService, private router: Router, private route: ActivatedRoute) {
-
-  }
-
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      email:new FormControl(null,[Validators.required, Validators.email]),
-      password: new FormControl(null , [Validators.required, Validators.minLength(6)])
-    })
-    this.route.queryParams.subscribe((params: Params) =>{
-      if(params['registered']){
-        MaterialService.toast('Теперь вы можете зайти в систему используя свои данные')
-
-      } else if (params['accessDenied']) {
-        MaterialService.toast('Для начала авторизуйтесь в системе')
-
-      }else if(params['sessionFiled']){
-        MaterialService.toast('Пожалуйста войдите в систему заного')
-      }
-    })
-  }
-  ngOnDestroy() {
-
-
-
-  }
-
-  onSubmit(){
-
-  }
+export class LoginPage {
 
 }
 
