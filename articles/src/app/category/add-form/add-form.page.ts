9e573c39da5940b9c265b9aea4df69f86e83fc4f
Denis Isachenko <55709662+lReWhite@users.noreply.github.com>
2019-11-03 23:38:59 +0300

    категории

--- articles/src/app/category/add-form/add-form.page.ts
+++ articles/src/app/category/add-form/add-form.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-add-form',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-form.page.scss'],
 })
 export class AddFormPage implements OnInit {
-
+  // @ts-ignore
+  @ViewChild('input') inputRef: ElementRef
   constructor() { }
-
+  image: File
+  imagePreview = ''
   ngOnInit() {
   }
 
+  triggerClick() {
+    this.inputRef.nativeElement.click()
+  }
+  onFileUpload(event: any) {
+
+    const file = event.target.files[0];
+    this.image = file;
+    const reader = new FileReader();
+    reader.onload = () => {
+
+
+
+      // @ts-ignore
+      this.imagePreview = reader.result;
+
+
+    };
+    reader.readAsDataURL(file);
+  }
 }
