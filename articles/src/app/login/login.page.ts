7fcc72bdaa9928606c14c4314c79a40d89d5d260
KiTKasatka <57257022+KiTKasatka@users.noreply.github.com>
2019-11-04 03:17:12 +0300

    Короче как-то так

--- articles/src/app/login/login.page.ts
+++ articles/src/app/login/login.page.ts
@@ -1,4 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {AuthService} from "../../../../client/src/app/shared/services/auth.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {MaterialService} from "../../../../client/src/app/shared/classes/material.service";
 
 
 @Component({
@@ -9,8 +14,46 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 export class LoginPage {
 povar = "https://i.ibb.co/m95gzWW/depositphotos-191208722-stock-illustration-cook-chef-logo-or-label.jpg"
 
+    form: FormGroup
+    aSub: Subscription
 
+    constructor(private auth:AuthService, private router: Router, private route: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            email:new FormControl(null,[Validators.required, Validators.email]),
+            password: new FormControl(null , [Validators.required, Validators.minLength(6)])
+        })
+        this.route.queryParams.subscribe((params: Params) =>{
+            if(params['registered']){
+                MaterialService.toast('Теперь вы можете зайти в систему используя свои данные')
+
+            } else if (params['accessDenied']) {
+                MaterialService.toast('Для начала авторизуйтесь в системе')
+
+            }else if(params['sessionFiled']){
+                MaterialService.toast('Пожалуйста войдите в систему заного')
+            }
+        })
+    }
+
+    ngOnDestroy() {
+        if(this.aSub){
+            this.aSub.unsubscribe()
+        }
+
+    }
 
     OnSubmit() {
         window.location.href="/observable";
+        this.form.disable()
+        // пока не понял в чем ошибка...
+        this.aSub=this.auth.login(this.form.value).subscribe(
+            () => this.router.navigate(['/overview']),
+            error => {
+                MaterialService.toast(error.error.message)
+
+                this.form.enable()
+            }
+        )
 }}
